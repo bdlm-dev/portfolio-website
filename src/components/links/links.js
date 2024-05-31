1ed041bb89d3eb55ec23cf021670f98ba88389c8
bdlm-dev <bdlm@hotmail.co.uk>
2024-05-31 14:28:23 +0100

    initial commit

---
+++ src/components/links/links.js
@@ -0,0 +1,5 @@
+const github = "https://github.com/bdlm-dev";
+const linkedin = "https://www.linkedin.com/in/april-miller-dev/";
+const email = "mailto:april@itisapril.dev";
+
+export {github, linkedin, email}
